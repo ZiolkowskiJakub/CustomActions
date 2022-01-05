3fb855a68acad244f30647c528a39c59d2115d53
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-01-05 20:37:07 +0100

    Initial Implementation

--- CustomActions/CustomAction.cs
+++ CustomActions/CustomAction.cs
@@ -1,5 +1,7 @@
 using Microsoft.Deployment.WindowsInstaller;
+using System;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace CustomActions
 {
@@ -8,8 +10,59 @@ namespace CustomActions
         [CustomAction]
         public static ActionResult UpdateRevitManifestFiles(Session session)
         {
-            session.Log("Begin CustomAction1");
-            MessageBox.Show("EEE");
+            string[] versions = new string[] { "2017", "2018", "2019", "2020", "2021", "2022" };
+
+            string mainManifestFileDirectory = Modify.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Autodesk", "Revit", "Addins");
+
+            //MessageBox.Show(string.Join(", ", session.CustomActionData.Keys));
+
+            if (!session.CustomActionData.TryGetValue("INSTALLDIR", out string installDirectory) || string.IsNullOrEmpty(installDirectory) || !System.IO.Directory.Exists(installDirectory))
+            {
+                return ActionResult.Failure;
+            }
+
+            //MessageBox.Show(installDirectory);
+
+            if (!session.CustomActionData.TryGetValue("MANIFESTFILENAME", out string manifestFileName) || string.IsNullOrEmpty(manifestFileName))
+            {
+                return ActionResult.Failure;
+            }
+
+            //MessageBox.Show(manifestFileName);
+
+            if (!session.CustomActionData.TryGetValue("ASSEMBLYNAME", out string assemblyName) || string.IsNullOrEmpty(assemblyName))
+            {
+                return ActionResult.Failure;
+            }
+
+            //MessageBox.Show(assemblyName);
+
+            foreach (string version in versions)
+            {
+                string manifestFilePath = System.IO.Path.Combine(mainManifestFileDirectory, version, manifestFileName);
+                if (!System.IO.File.Exists(manifestFilePath))
+                {
+                    continue;
+                }
+
+                string assemblyPath = System.IO.Path.Combine(installDirectory, string.Format("Revit {0}", version), assemblyName);
+                if (!System.IO.File.Exists(assemblyPath))
+                {
+                    continue;
+                }
+
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(manifestFilePath);
+
+                XmlNode xmlNode = xmlDocument.SelectSingleNode("/RevitAddIns/AddIn/Assembly");
+                if(xmlNode == null)
+                {
+                    continue;
+                }
+
+                xmlNode.InnerText = assemblyPath;
+                xmlDocument.Save(manifestFilePath);
+            }
 
             return ActionResult.Success;
         }
---
+++ CustomActions/Modify/CreateDirectory.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace CustomActions
+{
+
+    internal static partial class Modify
+    {
+        internal static string CreateDirectory(string directory, params string[] names)
+        {
+            string directory_Temp = directory;
+            foreach (string name in names)
+            {
+                string aDirectory_Temp = Path.Combine(directory_Temp, name);
+                if (!Directory.Exists(aDirectory_Temp))
+                    Directory.CreateDirectory(aDirectory_Temp);
+                directory_Temp = aDirectory_Temp;
+            }
+
+            return directory_Temp;
+        }
+    }
+
+}
